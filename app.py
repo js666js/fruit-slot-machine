d496d94a073f7e919bb9bbae8fec9acae1a7af4c
js666js <ma041224@icloud.com>
2026-03-08 10:13:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -165,5 +165,8 @@ def reset():
     session["winning_positions"] = []
     return redirect(url_for("index"))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
